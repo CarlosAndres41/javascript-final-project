e5efb0647d838519e3bc933cca851caecde67e6c
Carlos Andrés <ca.castaneda@outlook.com>
2023-06-08 17:52:31 -0500

    Add HTML structure

--- tesla-clone/src/components/Navbar.js
+++ tesla-clone/src/components/Navbar.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <div>
+            <div>
+                <img></img>
+            </div>
+            <div>
+                <ul>
+                    <li>Model S</li>
+                    <li>Model 3</li>
+                    <li>Model X</li>
+                    <li>Model Y</li>
+                    <li> Solar Roof</li>
+                    <li>Solar Panels</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>Shop</li>
+                    <li>Account</li>
+                    <li>Menu</li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
