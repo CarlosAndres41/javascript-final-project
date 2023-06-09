c6f9ca26e6a4be53c2468db48ed1ceaff6c560dc
Carlos Andrés <ca.castaneda@outlook.com>
2023-06-08 20:30:20 -0500

    Style button

--- tesla-clone/src/components/Navbar.js
+++ tesla-clone/src/components/Navbar.js
@@ -46,7 +46,9 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className='lg:hidden'>
-                <button>Menu</button>
+                <button className='inline-flex items-center py-2 px-4 text-sm font-medium bg-black/5 rounded-md shadow-sm hover:bg-black/10'>
+                    Menu
+                </button>
             </div>
         </div>
     );
