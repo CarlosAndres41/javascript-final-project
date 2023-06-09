40d0caffbfe9d74241cf3528be8f6637d6166ef3
Carlos Andrés <ca.castaneda@outlook.com>
2023-06-08 20:49:49 -0500

    Style h1

--- tesla-clone/src/components/Model.js
+++ tesla-clone/src/components/Model.js
@@ -5,7 +5,7 @@ const Model = () => {
     return (
         <div>
             <div className='absolute inset-x-0 top-[15%] text-center'>
-                <h1>Model 3</h1>
+                <h1 className='text-4xl font-bold'>Model 3</h1>
                 <p>Order Online for Touchless Delivery</p>
             </div>
             <div>
