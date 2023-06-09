f883585bfb262e7960b85e8ea8454ac17f96d989
Carlos Andrés <ca.castaneda@outlook.com>
2023-06-08 20:43:43 -0500

    Add HTML structure

--- tesla-clone/src/components/Model.js
+++ tesla-clone/src/components/Model.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Model = () => {
-    return <div>Model</div>;
+    return (
+        <div>
+            <div>
+                <h1>Model 3</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </div>
+            <div>
+                <button>Custom Order</button>
+                <button> Existing Inventory</button>
+            </div>
+            <div></div>
+        </div>
+    );
 };
 
 export default Model;
